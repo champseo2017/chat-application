3a051bdde5613a06743f07857c00d66fa861c117
boomgt <32265715+champseo2017@users.noreply.github.com>
2022-03-01 14:30:39 +0700

    10:22
    https://www.youtube.com/watch?v=a_xo-SbIfUQ

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,1 +1,19 @@
-console.log("this is from app https://www.youtube.com/watch?v=a_xo-SbIfUQ")
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+import cors from "cors";
+import config from "config";
+
+const port = config.get<number>("port");
+const host = config.get<string>("host");
+const corsOrigin = config.get<string>("corsOrigin");
+
+const app = express();
+
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    origin: corsOrigin,
+    credentials: true
+  },
+});

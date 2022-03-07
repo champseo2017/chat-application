69763d52836a5da2be94da88d9a0af7c4d981f2a
boomgt <32265715+champseo2017@users.noreply.github.com>
2022-03-07 22:53:40 +0700

    Build a Realtime Chat Application 32:48
    https://www.youtube.com/watch?v=a_xo-SbIfUQ

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,3 +1,4 @@
+import { useRef } from "react";
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
@@ -5,10 +6,37 @@ import styles from "../styles/Home.module.css";
 import { useSockets } from "../context/socket.context";
 
 import RoomsContainer from "../containers/Rooms";
+import MessagesContainer from "../containers/Messages";
 
 const Home: NextPage = () => {
-  const { socket } = useSockets();
-  return <div>{socket.id}fe</div>;
+  const { socket, username, setUsername } = useSockets();
+  const usernameRef = useRef<HTMLInputElement>(null);
+  function handleSetUsername() {
+    const value = usernameRef.current.value;
+    if (!value) {
+      return;
+    }
+    setUsername(value);
+    localStorage.setItem("username", value);
+  }
+  return (
+    <div>
+      {!username && (
+        <div className={styles.usernameWrapper}>
+          <div className={styles.usernameInner}>
+          <input placeholder="Username" ref={usernameRef} />
+          <button onClick={handleSetUsername}>START</button>
+          </div>
+        </div>
+      )}
+      {username && (
+        <div className={styles.container}>
+          <RoomsContainer />
+          <MessagesContainer />
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Home;
